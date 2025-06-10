7c140668ee93ec5134014891c5f0c3e91d6ca1b7
mdmhrz <razufreelance@gmail.com>
2025-06-10 20:23:57 +0600

    Category section updated

--- src/pages/CategoryNews.jsx
+++ src/pages/CategoryNews.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useLoaderData, useParams } from 'react-router';
 
 const CategoryNews = () => {
+    const [categoryNews, setCategoryNews] = useState([])
+    const data = useLoaderData();
+    const { id } = useParams();
+
+
+    useEffect(() => {
+
+        if (id == 0) {
+            setCategoryNews(data)
+            return
+        } else if (id == 1) {
+            const filteredNews = data.filter((news) => news.others.is_today_pick === true);
+            setCategoryNews(filteredNews);
+            return
+        } else {
+            const filteredNews = data.filter((news) => news.category_id == id);
+            setCategoryNews(filteredNews)
+
+        }
+
+
+    }, [data, id])
+
+
     return (
         <div>
-            Catgory news
+            Total {categoryNews.length} found
         </div>
     );
 };
