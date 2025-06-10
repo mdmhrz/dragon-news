aaf903c5eb5387646299f0187cd5ecf8a52778b7
mdmhrz <razufreelance@gmail.com>
2025-06-10 15:43:24 +0600

    custom theme added from daisy ui and data format npm added form date fns

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from '../assets/logo.png'
+import { format } from 'date-fns';
 
 const Header = () => {
     return (
-        <div className='flex items-center justify-center flex-col gap-3'>
-            <img className='w-[350px]' src={logo} alt="" />
-            <p>Journalism without fear & favour</p>
+        <div className='flex items-center justify-center flex-col gap-3 mt-5'>
+            <img className='w-[400px]' src={logo} alt="" />
+            <p className='text-accent'>Journalism without fear & favour</p>
+            <p className='font-semibold'>{format(new Date(), 'EEEE,')}<span className='text-accent'>{format(new Date(), ' MMMM MM, YYY')}</span></p>
         </div>
     );
 };
